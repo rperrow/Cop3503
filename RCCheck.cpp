3119b0f8776d6ae38407587a30af0e2c1bde37e5
vgarbaran <vgarbaran@ufl.edu>
2015-12-05 18:14:41 -0500

    Update RCCheck.cpp

--- RCCheck.cpp
+++ RCCheck.cpp
@@ -12,55 +12,32 @@ bool Cube::CheckColors(){ // checks to see if the inputted cube has the appropri
 	int white = 0;
 	
 // loop to go through and access each tile in the cube, summing up the number of each color
-	for (int i = 0; i < 48; i++)
-	{
-		if (inputs[i] == "G"){
-			green++;
-		}
-		if (inputs[i] == "R"){
-			red++;
-		}
-		if (inputs[i] == "B"){
-			blue++;
-		}
-		if (inputs[i] == "O"){
-			orange++;
-		}
-		if (inputs[i] == "Y"){
-			yellow++;
-		}
-		if (inputs[i] == "W"){
-			white++;
-		}
-	}
-
-	if ( (green == 8) && (red == 8) && (blue == 8) && (orange == 8) && (yellow == 8) && (white == 8) )  // checks to see if all the colors adds up to the appropriate amount
-		return true;
-	else
-		return false;
-}
-bool Cube::CheckRepeats(){  // checks to see if any of the same color share the same relative positioning
-
-// first three for loops isolates a cell to be compared
 	for (int k = 0; k < 6; k++)
 	{
 		for (int i = 0; i < 3; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
-					for (int m = 0; m < 6; m++) // last for loop is then used to compare to other relative positions
-						{
-							if (m != k)  // case when you are comparing an object to itself will of course result in a "repeat" thus is excluded
-							{
-								if (this->cubeArr[k].getFace()[i][k].getColor() == this->cubeArr[m].getFace()[i][k].getColor())
-									return false;
-							}
-						}
+					if (this->cubeArr[k].getFace()[i][j].getColor() == "G")
+						green++;
+					else if (this->cubeArr[k].getFace()[i][j].getColor() == "R")
+						red++;
+					else if (this->cubeArr[k].getFace()[i][j].getColor() == "B")
+						blue++;
+					else if (this->cubeArr[k].getFace()[i][j].getColor() == "O")
+						orange++;
+					else if (this->cubeArr[k].getFace()[i][j].getColor() == "Y")
+						yellow++;
+					else if (this->cubeArr[k].getFace()[i][j].getColor() == "W")
+						white++;
 				}
 			}
 	}
-	
-	return true;
+
+	if ( (green == 9) && (red == 9) && (blue == 9) && (orange == 9) && (yellow == 9) && (white == 9) )  // checks to see if all the colors adds up to the appropriate amount
+		return true;
+	else
+		return false;
 }
 bool Cube::CheckSides(){  //checks to see if a side piecs has the same color on both sides
 	
@@ -89,25 +66,25 @@ bool Cube::CheckSides(){  //checks to see if a side piecs has the same color on 
 // second four for loops compares the back sides to see if any two colors share the same edge
 	for (int i=0; i < 3; i++)
 	{
-		if (this->cubeArr[3].getFace()[0][i].getColor() == this->cubeArr[0].getFace()[0][i].getColor())
+		if (this->cubeArr[3].getFace()[0][i].getColor() == this->cubeArr[0].getFace()[0][2-i].getColor())
 			return false;
 	}
 	
 	for (int i=0; i < 3; i++)
 	{
-		if (this->cubeArr[3].getFace()[2][i].getColor() == this->cubeArr[5].getFace()[2][i].getColor())
+		if (this->cubeArr[3].getFace()[2][i].getColor() == this->cubeArr[5].getFace()[2][2-i].getColor())
 			return false;
 	}
 	
 	for (int i=0; i < 3; i++)
 	{
-		if (this->cubeArr[3].getFace()[i][0].getColor() == this->cubeArr[2].getFace()[i][2].getColor())
+		if (this->cubeArr[3].getFace()[i][0].getColor() == this->cubeArr[4].getFace()[i][2].getColor())
 			return false;
 	}
 	
 	for (int i=0; i < 3; i++)
 	{
-		if (this->cubeArr[3].getFace()[i][2].getColor() == this->cubeArr[4].getFace()[i][0].getColor())
+		if (this->cubeArr[3].getFace()[i][2].getColor() == this->cubeArr[2].getFace()[i][0].getColor())
 			return false;
 	}
 
@@ -124,7 +101,7 @@ bool Cube::CheckSides(){  //checks to see if a side piecs has the same color on 
 	return true;
 }
 bool Cube::Valid(){  // combines the previous functions to evaluate if the overall cube is valid or not
-	if ((this->CheckColors() == true) && (this->CheckRepeats() == true) && (this->CheckSides() == true))  // checks to see if the cube passes all of the tests and returns true if the cube does
+	if ((this->CheckColors() == true) && (this->CheckSides() == true))  // checks to see if the cube passes all of the tests and returns true if the cube does
 		return true;
 	else  // if it doesn't the functions returns false
 		return false;
